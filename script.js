9d5f8f4f3148e787e91122c7088a08bc9b4188af
git-mahade <git-mahade@devxhub.com>
2024-02-18 14:39:54 +0600

    main

--- script.js
+++ script.js
@@ -21,39 +21,6 @@ const buttonRipple = mdc.ripple.MDCRipple.attachTo(
   document.querySelector(".mdc-button")
 );
 
-// // Initialize data table
-// const dataTable = mdc.dataTable.MDCDataTable.attachTo(
-//   document.querySelector(".mdc-data-table")
-// );
-// dataTable.listen("MDCDataTable:sorted", (event) => {
-//   const { columnId, sortValue } = event.detail;
-//   // Implement your sorting logic here
-//   // For instance, sort your data source based on `columnId` and `sortValue` (ascending or descending)
-//   console.log(`Column sorted: ${columnId}, Direction: ${sortValue}`);
-// });
-
-// //initialze modal
-// const dialog = mdc.dialog.MDCDialog.attachTo(
-//   document.querySelector(".mdc-dialog")
-// );
-
-// // Initialize the list inside the dialog
-// const list = mdc.list.MDCList.attachTo(
-//   document.querySelector(".mdc-dialog .mdc-list")
-// );
-
-// // Listen for the dialog to be opened and lay out the list
-// dialog.listen("MDCDialog:opened", () => {
-//   list.layout();
-// });
-
-// // Find the button and attach a click event listener to open the dialog
-// document
-//   .querySelector(".open-dialog-button")
-//   .addEventListener("click", function () {
-//     dialog.open();
-//   });
-
 // Initialize menu
 const menu = new mdc.menu.MDCMenu(document.querySelector(".mdc-menu"));
 const adminButton = document.getElementById("adminButton");
